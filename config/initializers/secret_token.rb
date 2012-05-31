5a1cd74eb02d98263a8fc6086b09bd312eed0324
Matthew Ford <matt@bitzesty.com>
2012-05-31 22:47:58 +0100

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Willitiframe::Application.config.secret_token = '3dbfb1b470b636364bf474b38138e7eb6df8518b39f7c7faf2783b4b3ef7ad643b70260470f9785738e7b6a270f09b007bc3609a58fd1205c747412dadac9287'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Willitiframe::Application.config.session_store :cookie_store, key: '_willitiframe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Willitiframe::Application.config.session_store :active_record_store
